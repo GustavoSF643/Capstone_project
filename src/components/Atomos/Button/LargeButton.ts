aa87a48c7bb55705fbbda205cf137524ebf2db92
marquelly <mmssouza1@gmail.com>
2021-07-07 18:12:05 -0300

    feat - LargeButton

---
+++ src/components/Atomos/Button/LargeButton.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const LargeButton = styled.button`
+    width: 450px; 
+    height: 70px;
+    background-color: #694B3C; 
+    color: white; 
+    border-radius: 10px; 
+    font-size: 24px;
+    font-family: Roboto, 'Open Sans', sans-serif;
+    cursor: pointer; 
+`;
+
