d81bce695d7422ec85a7f77c92d8be29feecc302
marquelly <mmssouza1@gmail.com>
2021-07-08 16:25:00 -0300

    feat - footer styled

--- src/components/Molecules/Footer/style.ts
+++ src/components/Molecules/Footer/style.ts
@@ -3,5 +3,17 @@ import styled from "styled-components";
 export const FooterDiv = styled.div`
     background-color: #694B3C; 
     color: white; 
-    
+    display: flex;
+    align-items: left;
+    justify-content: space-evenly;
+    width: 600px;
+    a {
+        font-size: 24px;
+        font-weight: bold;
+        color:#FFFFFF;
+        text-decoration: none;
+    }   
+
 `;
+
+
