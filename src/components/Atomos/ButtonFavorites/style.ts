82113fee6e7f58332e639393488d5be963bb66b7
Jessica Shiotani Arasake <jessyarasake@hotmail.com>
2021-07-08 10:40:00 -0300

    chore: solve conflicts

--- src/components/Atomos/ButtonFavorites/style.ts
+++ src/components/Atomos/ButtonFavorites/style.ts
@@ -4,7 +4,7 @@ export const Button = styled.button`
   display: inline-block;
   height: 55px;
   padding: 12px 12px;
-  background-color: rgb(255, 255, 255, 0.4);
+  background-color: var(--sixth);
   border-radius: 50%;
   box-sizing: border-box;
   font-size: 2rem;
