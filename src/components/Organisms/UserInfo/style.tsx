c9a155b25df6bdc85198b11136c51b717347c725
Gustavo Silva <gustavosilvafranco643@gmail.com>
2021-07-08 18:35:06 -0300

    feat: UserInfo style ajusted

--- src/components/Organisms/UserInfo/style.tsx
+++ src/components/Organisms/UserInfo/style.tsx
@@ -3,11 +3,16 @@ import styled from "styled-components";
 export const UserInfoDiv = styled.div`
   display: flex;
   align-items: center;
+  justify-content: left;
+  border-bottom: 2px solid var(--third);
+  padding: 50px;
+  position: relative;
 `;
 
 export const InfoImageDiv = styled.div`
   & > img {
     border-radius: 100%;
+    width: 250px;
   }
 `;
 
@@ -16,4 +21,19 @@ export const InfoDiv = styled.div`
   flex-direction: column;
   align-items: left;
   color: var(--third);
+  margin: 50px;
+
+  & > h3 {
+    font-size: 32px;
+  }
+
+  & > p {
+    font-size: 24px;
+  }
+`;
+
+export const ButtonDiv = styled.div`
+  position: absolute;
+  right: 30px;
+  bottom: 5px;
 `;
