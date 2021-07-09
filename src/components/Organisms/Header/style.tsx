c9f61f121f1621b50147d6a1b5ca28526c9133b8
Gustavo Silva <gustavosilvafranco643@gmail.com>
2021-07-09 16:58:01 -0300

    feat: styled and UserMenu ajusted

--- src/components/Organisms/Header/style.tsx
+++ src/components/Organisms/Header/style.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const HeaderDiv = styled.div`
   position: fixed;
+  top: 0;
   width: 100vw;
   display: flex;
   justify-content: center;
--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,7 +1,65 @@
+import { useState, useEffect } from "react";
+import { Redirect } from "react-router";
 import UserInfo from "../../components/Organisms/UserInfo";
+import { UserDiv, UserMenuDiv } from "./style";
+import jwt_decode from "jwt-decode";
+import { DecodedProps } from "../../types/DecodedProps";
 
 const User = () => {
-  return <UserInfo userId="10"></UserInfo>;
+  const [userId, setUserId] = useState<string>("");
+  const [token] = useState<string | null>(
+    JSON.stringify(localStorage.getItem("@petMacher:token") || null)
+  );
+  const [infoOpened, setInfoOpened] = useState(true);
+  const [favoritesOpened, setFavoritesOpened] = useState(false);
+  const [petsOpened, setPetsOpened] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      const decode: DecodedProps = jwt_decode(token);
+      setUserId(decode.sub);
+    }
+  }, []);
+
+  const changeMenu = (name: string) => {
+    if (name === "info") {
+      setInfoOpened(true);
+    } else {
+      setInfoOpened(false);
+    }
+    if (name === "favorites") {
+      setFavoritesOpened(true);
+    } else {
+      setFavoritesOpened(false);
+    }
+    if (name === "pets") {
+      setPetsOpened(true);
+    } else {
+      setPetsOpened(false);
+    }
+  };
+
+  if (!token) {
+    return <Redirect to="/register" />;
+  }
+
+  return (
+    <UserDiv>
+      <h3>Perfil</h3>
+      <UserMenuDiv
+        infoOpened={infoOpened}
+        favoritesOpened={favoritesOpened}
+        petsOpened={petsOpened}
+      >
+        <button onClick={() => changeMenu("info")}>Informações</button>
+        <button onClick={() => changeMenu("favorites")}>Favoritos</button>
+        <button onClick={() => changeMenu("pets")}>Seus Pets</button>
+      </UserMenuDiv>
+      {infoOpened && userId && <UserInfo userId={userId}></UserInfo>}
+      {favoritesOpened && <div>Favoritos</div>}
+      {petsOpened && <div>Seus Pets</div>}
+    </UserDiv>
+  );
 };
 
 export default User;
