fa42e25cafded62d928dc776947a047e8fe7bef0
marquelly <mmssouza1@gmail.com>
2021-07-10 07:58:43 -0300

    feat - footer add

--- src/components/Molecules/Footer/index.tsx
+++ src/components/Molecules/Footer/index.tsx
@@ -1,10 +1,35 @@
-import { FooterDiv } from "./style";
+import { FooterDiv, Div, Icons} from "./style";
+import LogoFooter from "../../../assets/LogoFooter.png"; 
+import { FaFacebook, FaInstagram, FaTwitter} from "react-icons/fa";
 
 const Footer = () => {
   return (
+   <>   
+     
     <FooterDiv>
-        <a href="">teste </a>       
+       <Div>  
+         <img  src={LogoFooter} />
+         <a href="">Contato</a> 
+         <a href="">sobre nós</a>    
+      </Div>
+      <Div>  
+         <h3>Assine a nossa newsletter</h3>
+         <input type="text" placeholder="Nome"/>
+         <input type="text" placeholder="email"/> 
+         <button> Cadastrar </button>
+      </Div> 
+      <Div>  
+         <h3>Siga nossa página nas redes sociais</h3>
+         <Icons>
+            <div>  
+               <FaFacebook/>
+               <FaInstagram/>
+               <FaTwitter/>
+            </div>  
+         </Icons>   
+      </Div>                   
     </FooterDiv>
+   </> 
   );
 };
 
