01bef0bcd708aaf4f0eeeeb9dbb6ef198c01b91f
Gustavo Silva <gustavosilvafranco643@gmail.com>
2021-07-12 11:28:47 -0300

    states ajusted with useHistory

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -4,15 +4,41 @@ import UserInfo from "../../components/Organisms/UserInfo";
 import { UserDiv, UserMenuDiv } from "./style";
 import jwt_decode from "jwt-decode";
 import { DecodedProps } from "../../types/DecodedProps";
+import { useHistory } from "react-router-dom";
+
+interface StateProps {
+  message: string;
+}
+
+interface LocationProps {
+  state: StateProps;
+}
+
+interface HistoryProps {
+  location: LocationProps;
+}
 
 const User = () => {
+  const history: HistoryProps = useHistory();
+  const {
+    location: {
+      state: { message },
+    },
+  } = history;
+
   const [userId, setUserId] = useState<string>("");
   const [token] = useState<string | null>(
     JSON.stringify(localStorage.getItem("@petMacher:token") || null)
   );
-  const [infoOpened, setInfoOpened] = useState(true);
-  const [favoritesOpened, setFavoritesOpened] = useState(false);
-  const [petsOpened, setPetsOpened] = useState(false);
+  const [infoOpened, setInfoOpened] = useState<boolean>(() => {
+    return message === "info" ? true : false;
+  });
+  const [favoritesOpened, setFavoritesOpened] = useState<boolean>(() => {
+    return message === "favorites" ? true : false;
+  });
+  const [petsOpened, setPetsOpened] = useState<boolean>(() => {
+    return message === "pets" ? true : false;
+  });
 
   useEffect(() => {
     if (token) {
