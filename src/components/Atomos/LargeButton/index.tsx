4c02696b7d4faa0834e3651f73560a4109711864
Jessica Shiotani Arasake <8128240-jessyshiotani@users.noreply.gitlab.com>
2021-07-12 16:51:05 +0000

    fix(large button): change text display in large button

--- src/components/Atomos/LargeButton/index.tsx
+++ src/components/Atomos/LargeButton/index.tsx
@@ -1,18 +1,18 @@
 import { LargeButtonStyled } from "./styles";
-import patinha_botao from "../../../assets/patinha_botao.png"; 
+import patinha_botao from "../../../assets/patinha_botao.png";
 import { ReactNode } from "react";
 
-interface InputProps { 
-    children: ReactNode;
-    [x:string]: any;
+interface InputProps {
+  children: ReactNode;
+  [x: string]: any;
 }
-  
+
 const LargeButton = ({ children }: InputProps) => {
   return (
-      <LargeButtonStyled>
-          {children} <img src={patinha_botao} />     
-      </LargeButtonStyled>
-  )    
-}
-  
+    <LargeButtonStyled>
+      {children} <img src={patinha_botao} />
+    </LargeButtonStyled>
+  );
+};
+
 export default LargeButton;
