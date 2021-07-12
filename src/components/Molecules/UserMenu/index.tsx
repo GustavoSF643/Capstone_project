f574ae0c233fabbb04b50fff87f0378e07924c41
Gustavo Silva <gustavosilvafranco643@gmail.com>
2021-07-12 14:01:24 +0000

    Feat/user menu

--- src/components/Molecules/UserMenu/index.tsx
+++ src/components/Molecules/UserMenu/index.tsx
@@ -1,13 +1,29 @@
+import { useHistory } from "react-router-dom";
 import { UserMenuDiv } from "./style";
 
 const UserMenu = () => {
+  const history = useHistory();
+
   return (
     <UserMenuDiv>
       <h3> Usuário </h3>
-      <a>Informações</a>
-      <a>Favoritos</a>
-      <a>Seus pets</a>
-      <a>Sair</a>
+      <a onClick={() => history.push("/user", { message: "info" })}>
+        Informações
+      </a>
+      <a onClick={() => history.push("/user", { message: "favorites" })}>
+        Favoritos
+      </a>
+      <a onClick={() => history.push("/user", { message: "pets" })}>
+        Seus pets
+      </a>
+      <a
+        onClick={() => {
+          localStorage.clear();
+          history.push("/");
+        }}
+      >
+        Sair
+      </a>
     </UserMenuDiv>
   );
 };
